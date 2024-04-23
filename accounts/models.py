23d89335fdcd97b80b0c21683ca4c72c1d2ed34d
Lenis <fardintorkamand83@gmail.com>
2024-04-24 01:33:29 +0330

    Create custom user model and introduce it to django

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    """
+        A CustomUser model for manage your website users
+    """
+    username = None
+    phone_number = models.CharField(max_length=11, unique=True)
+    otp_code = models.PositiveSmallIntegerField(blank=True, null=True)
+    otp_code_created = models.DateTimeField(auto_now_add=True)
+
+    # Other favorite fields...
+
+    objects = None
+
+    USERNAME_FIELD = 'phone_number'
+
+    REQUIRED_FIELDS = []
+
+    backend = ''
